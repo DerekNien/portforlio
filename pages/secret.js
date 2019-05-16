38066b088d63a6c405c6c8d97e49a9ecdd18b14a
DerekNien <aegis60918@gmail.com>
2019-05-16 22:58:56 +0800

    - Add: data for secret

--- pages/secret.js
+++ pages/secret.js
@@ -4,12 +4,41 @@ import BasePage from '../components/layouts/BasePage';
 
 import withAuth from '../components/hoc/withAuth';
 
+import axios from 'axios';
+
 class Secret extends Component {
+  state = {
+    secretData: [],
+  };
+
+  async componentDidMount() {
+    const res = await axios.get('/api/v1/secret');
+    const secretData = res.data;
+    this.setState({
+      secretData,
+    });
+  }
+
+  displaySecretData = () => {
+    const { secretData } = this.state;
+    if (secretData && secretData.length > 0) {
+      return secretData.map((data, index) => (
+        <div key={index}>
+          <p>{data.title}</p>
+          <p>{data.description}</p>
+        </div>
+      ));
+    }
+
+    return null;
+  };
+
   render() {
     return (
       <BaseLayout {...this.props.auth}>
         <BasePage>
           <h1>Secret</h1>
+          {this.displaySecretData()}
         </BasePage>
       </BaseLayout>
     );
